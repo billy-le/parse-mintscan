40b838e72b3201f0f80a68e8c932ae20d6ec9c79
Billy Le <hi@billyle.dev>
2023-10-23 22:51:08 -0700

    fixed multi-vote expenses

--- processors/legacyVote.ts
+++ processors/legacyVote.ts
@@ -3,16 +3,19 @@ import { getFees } from "../utils/getFees";
 
 export async function legacyVote(tx: Tx, logs: Log[]) {
   const transactions: Partial<Transaction>[] = [];
+  const proposalIds = [];
   for (const log of logs) {
     const voteAttributes =
       log.events.find(({ type }) => type === "proposal_vote")?.attributes ?? [];
     const proposalId = getValueOfKey(voteAttributes, "proposal_id")?.value;
-
-    transactions.push({
-      type: "Expense",
-      feeAmount: await getFees(tx),
-      description: `Vote on #${proposalId}`,
-    });
+    if (proposalId) {
+      proposalIds.push(proposalId);
+    }
   }
+  transactions.push({
+    type: "Expense",
+    feeAmount: await getFees(tx),
+    description: `Vote on #${proposalIds.join(" #")}`,
+  });
   return transactions;
 }
--- processors/msgVote.ts
+++ processors/msgVote.ts
@@ -3,18 +3,22 @@ import { getFees } from "../utils/getFees";
 
 export async function msgVote(baseSymbol: string, tx: Tx, logs: Log[]) {
   const transactions: Partial<Transaction>[] = [];
+  const proposalIds = [];
   for (const log of logs) {
     const voteAttributes =
       log.events.find(({ type }) => type === "proposal_vote")?.attributes ?? [];
     const proposalId = getValueOfKey(voteAttributes, "proposal_id")?.value;
-
-    transactions.push({
-      type: "Expense",
-      feeAmount: await getFees(tx),
-      feeAsset: baseSymbol,
-      description: `Vote on #${proposalId}`,
-    });
+    if (proposalId) {
+      proposalIds.push(proposalId);
+    }
   }
+
+  transactions.push({
+    type: "Expense",
+    feeAmount: await getFees(tx),
+    feeAsset: baseSymbol,
+    description: `Vote on #${proposalIds.join(" #")}`,
+  });
 
   return transactions;
 }
